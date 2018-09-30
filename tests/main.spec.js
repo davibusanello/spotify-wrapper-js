1e42bc99281949db985f9e2e6e0926d6ae13dc12
Davi Busanello <itsme@davibusanello.net>
2018-09-30 18:43:05 -0300

    Smoke tests

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,6 +1,29 @@
 import { expect } from 'chai';
+import {
+  search, searchAlbums, searchArtists, searchTracks, searchPlaylists,
+} from '../src/main';
 
-describe('Index', () => {
+describe('Spotify Wrapper', () => {
   // smoke tests
+  describe('Smoke tests', () => {
+    it('should exist the search method', () => {
+      expect(search).to.exist;
+    });
 
+    it('should exist the searchAlbums method', () => {
+      expect(searchAlbums).to.exist;
+    });
+
+    it('should exist the searchArtists method', () => {
+      expect(searchArtists).to.exist;
+    });
+
+    it('should exist the searchTracks method', () => {
+      expect(searchTracks).to.exist;
+    });
+
+    it('should exist the searchPlaylists method', () => {
+      expect(searchPlaylists).to.exist;
+    });
+  });
 });
