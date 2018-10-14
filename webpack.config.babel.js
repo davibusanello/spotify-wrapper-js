55acb25cbf997db19bb7153400642753a5b2bca5
Davi Busanello <itsme@davibusanello.net>
2018-10-13 23:49:50 -0300

    Add webpack and preparing to use Travis

---
+++ webpack.config.babel.js
@@ -0,0 +1,25 @@
+import { join } from 'path';
+
+export default {
+  entry: './index',
+  output: {
+    path: join(__dirname, 'dist'),
+    libraryTarget: 'umd',
+    library: 'spotifyWrapper',
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        // exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+};
